4156e85a11be03f3dd289d50f7b4c64dc7327c75
Navnit P <navnit3366@gmail.com>
2023-12-02 09:40:24 +0530

    PointsBot-main

---
+++ config.py
@@ -0,0 +1,5 @@
+Token = "TOKEN"
+Prefix = '.'
+PointsName = "Goon Tokens"
+Log_channel = 821442828056002602 # Channel where all shop purchases are logged.
+POINTS_TO_BE_GIVEN_PER_MESSAGE = 2 # Points which will be given to members per message.
